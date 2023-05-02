73b357988544691f8e397bd6602b87548dd138e2
qwr133 <121946724+qwr133@users.noreply.github.com>
2023-05-02 17:41:59 +0900

    서브쿼리

--- mariadb_score 테이블.sql
+++ mariadb_score 테이블.sql
@@ -48,7 +48,42 @@ order by board_no desc
 -- --index, 양
 limit 0, 10 
 
-
+;
 
        SELECT COUNT(*)
-        FROM tbl_board
+        FROM tbl_board;
+        
+select *
+from tbl_board 
+-- --검색기능 추가
+where title like concat('%', '30','%') 
+or content like '%30'
+order by board_no desc 
+limit 0, 6 
+-- 1페이지 보여줘, 6,12 2페이지 보여줘
+;
+
+
+create table tbl_reply(
+	reply_no INT(10) auto_increment,
+	reply_text VARCHAR(1000) not null,
+	reply_writer VARCHAR(100) not null,
+	reply_date DATETIME default current_timestamp,
+	board_no int(10),
+-- 1대 다 관계로 게시글 번호가 필수적으로 필요함 (PK)
+	constraint pk_reply primary key (reply_no),
+	constraint fk_reply
+	foreign key (board_no)
+	references tbl_board(board_no)
+-- 	글 삭제하면 댓글도 다 삭제됨, 이거 없으면 댓글 있으면 게시물 다 삭제됨
+-- 	--실무적으론 백업테이블에 넣어두고, 삭제하는 방식으로 하게 됨 - 조심해야함
+	on delete cascade
+);
+-- 1
+drop table tbl_reply  ;
+-- 2 - 3>create table
+truncate table tbl_board ;
+
+select * from tbl_board;
+select * from tbl_reply
+where board_no=3;
